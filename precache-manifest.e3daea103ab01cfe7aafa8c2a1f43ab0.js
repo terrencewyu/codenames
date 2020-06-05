612bbb7d4fdc08816a04a2af12b2245221eb4fea
Terrence Yu <tyu@xmatters.com>
2020-06-05 15:45:03 -0700

    Updates

---
+++ precache-manifest.e3daea103ab01cfe7aafa8c2a1f43ab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b76ac398a8a59a3c33b0ccbe7c893b1f",
+    "url": "/codenames/index.html"
+  },
+  {
+    "revision": "111b8242dc1c14e16c0f",
+    "url": "/codenames/static/css/main.8678bf15.chunk.css"
+  },
+  {
+    "revision": "7c85c8fce270c8bfe77c",
+    "url": "/codenames/static/js/2.02fd8e9b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codenames/static/js/2.02fd8e9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111b8242dc1c14e16c0f",
+    "url": "/codenames/static/js/main.9f8d264b.chunk.js"
+  },
+  {
+    "revision": "47f9a28400cb49725c32",
+    "url": "/codenames/static/js/runtime-main.5587d490.js"
+  }
+]);
